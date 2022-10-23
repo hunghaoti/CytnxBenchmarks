5d2fe51b0933b111cc3eb47e54066fa82ff8ffc6
hunghaoti <hunghaoti@localhost.localdomain>
2022-10-24 01:04:15 +0800

    fix the problem when itensor compiled in mkl platform, cannot include cytnx and itensor header file in the same time.

--- google_benchmark/basic_test.cpp
+++ google_benchmark/basic_test.cpp
@@ -6,20 +6,12 @@
 // Cytnx test
 static void BM_Cytnx_declare(benchmark::State& state)
 {
-    for (auto _: state) {
-	cytnx::Tensor A;
-    }
+	for (auto _: state) 
+	{
+		cytnx::Tensor A;
+	}
 }
 BENCHMARK(BM_Cytnx_declare);
-
-static void BM_Cytnx_reshape(benchmark::State& state)
-{
-    for (auto _: state) {
-	    auto A = cytnx::arange(24);
-	    auto B = A.reshape(2, 3, 4);
-    }
-}
-BENCHMARK(BM_Cytnx_reshape);
 
 static void BM_Cytnx_contract(benchmark::State& state)
 {
@@ -29,19 +21,35 @@ static void BM_Cytnx_contract(benchmark::State& state)
 		auto B = cytnx::UniTensor(cytnx::ones({3, 3, 3, 3}));
 		B.set_labels(std::vector<long int>{2l, 3l, 4l, 5l});
 		auto C = cytnx::Contract(A, B);
-    }
+	}
 }
 BENCHMARK(BM_Cytnx_contract);
+
+//test with several arguments, ex, bond dimension
+static void TestBenchmark(benchmark::State& state) 
+{
+	int i = state.range(0);
+	int j = state.range(1);
+	for (auto _ : state) 
+	{
+		auto A = cytnx::UniTensor(cytnx::ones({i, i, 3}));
+	}
+}
+BENCHMARK(TestBenchmark)
+	->Args({5, 3})
+	->Args({10, 9});
+;
 
 // itensor test
 static void BM_itensor_declare(benchmark::State& state)
 {
-    for (auto _: state) {
-	    auto i = itensor::Index(4, "index i");
-	    auto j = itensor::Index(6, "index j");
-	    auto T = itensor::ITensor(i,j);
-	    T.set(i=3, j = 2, 3.14159);
-    }
+	for (auto _: state) 
+	{
+		auto i = itensor::Index(4, "index i");
+		auto j = itensor::Index(6, "index j");
+		auto T = itensor::ITensor(i,j);
+		T.set(i=3, j = 2, 3.14159);
+	}
 }
 BENCHMARK(BM_itensor_declare);
  
